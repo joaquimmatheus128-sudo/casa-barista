01df5e1bed8882802859dea84c80dbf572c23715
joaquimmatheus128-sudo <joaquim.matheus@gmail.com>
2026-07-27 16:58:56 -0300

    Configuração do bando de dados para o banner na home

--- src/app/Http/Controllers/Site/HomeController.php
+++ src/app/Http/Controllers/Site/HomeController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 
 class HomeController extends Controller{
 
     // Metodo HOME - Carregar a INDEX (HOME)
     public function home(){
-        
-        return view('site.home.home');
+    
+        //Busque a lista de banners para exibir na home(Views)
+        $listaBanner = Banner::where('status_banner', 'ATIVO')->inRandomOrder()->get();
+
+        //dd($listaBanner);
+        //var_dump($listaBanner);
+
+        return view('site.home.home', compact('listaBanner'));
     
     }
 
